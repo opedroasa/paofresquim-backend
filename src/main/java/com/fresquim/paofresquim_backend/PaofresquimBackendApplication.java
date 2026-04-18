98501dcaef606c17b9482c7a653f5027a76299cf
Pedro Alexandre <pedroantonio2502@gmail.com>
2026-04-18 14:41:08 -0300

    Initial commit

---
+++ src/main/java/com/fresquim/paofresquim_backend/PaofresquimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fresquim.paofresquim_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaofresquimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaofresquimBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fresquim/paofresquim_backend/PaofresquimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fresquim.paofresquim_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaofresquimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
